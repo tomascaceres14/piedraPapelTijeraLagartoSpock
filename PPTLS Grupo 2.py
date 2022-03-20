defe18361ead1b5452b99a98bf159f043cdbc01f
Tomas Alegria Caceres <tomialegriacaceres@gmail.com>
2022-03-20 15:04:04 -0300

    Mejora en los comentarios

--- PPTLS Grupo 2.py
+++ PPTLS Grupo 2.py
@@ -31,13 +31,10 @@ data = open('analiticas.json', 'r')
 ## y esa informacion, dentro del json.loads() la deserializamos a formato diccionario.
 analiticas = json.loads(data.read())
 
+## Bucle while True, para que al finalizar cada ronda, vuelva a ejecutarse de cero. Para finalizar programa o bucle, tenemos se usa 'break'
 while True:
 
     ## Menu
-
-    ## Computadora elige numero aleatorio desde el 0 hasta (sin incluir) el 5
-    aleatorio = random.randrange(0, 5)
-
     print("\n")
     print("0) Piedra")
     print("1) Papel")
@@ -53,7 +50,7 @@ while True:
         opcion = int(input("Que eliges: ")) ## Aclaramos que el input debe ser de tipo int.
         print("\n#####################\n")
     except: ##Si el user ingresa una letra o signo, el try/except hace que no rompa el codigo, y en su lugar
-        print("\n#####################") ## imprima que el valor es invalido. 
+        print("\n#####################") ## imprima una advertencia. 
         print("\nValor invalido! Se debe ingresar un numero del 1 al 5")
         continue ## Con el continue reiniciamos el ciclo While.
 
@@ -100,27 +97,29 @@ while True:
     ## Eleccion del menu
 
     if opcion in range(0, 5):
-        eligeUsuario = elementos[opcion]
+        eligeUsuario = elementos[opcion] # Relacion directa entre el indice del elemento en la lista (array) con valor ingresado en input
     elif opcion == 5:
+        ## Menu analiticas
         print("Cantidad de partidas:", analiticas["partidas"])
         print("Cantidad de victorias:", analiticas["victorias"])
         print("Cantidad de derrotas:", analiticas["derrotas"])
-        try:
+        try: ## Esto esta en caso de el programa no caiga si el usuario no tiene victorias, ya que intenta dividir por 0. 
             print("Winrate:", round(calculoPorcentaje(analiticas["victorias"], analiticas["partidas"])),"%")
         except:
-            print("Winrate: 0%. Error en division por cero. Una sola partida te pido que ganes....")
+            print("Winrate: 0%. Error en division por cero. Una sola te pido que ganes....")
         continue
     elif opcion == 6:
         print("nos vemos!")
-        break
+        break ## Rompe el bucle While y finaliza el programa
     else:
         print("Por favor, ingrese un valor valido")
         continue
 
     print("Tu eliges: ", eligeUsuario["nombre"])
 
-    ## Definimos eleccion de la PC
-    
+    ## Eleccion de la PC
+    ## Computadora elige numero aleatorio desde el 0 hasta (sin incluir) el 5
+    aleatorio = random.randrange(0, 5)
     eligePc = elementos[aleatorio]
 
     print("PC eligio: ", eligePc["nombre"])
@@ -128,22 +127,24 @@ while True:
  
     ## Comparaciones
     
-    i = 0
+    i = 0 ## Variable para iterar sobre arrays a cada vuelta de bucle for
 
-    for num in eligeUsuario["ganaContra"]:
+    ## Logica: 
+    ## Cada elemento tiene un id y dos listas de id's de otros elementos; Una de elementos derrotables y otra de elementos que lo derrotan.
+    for num in eligeUsuario["ganaContra"]: ## Iteramos sobre dicha lista de posibles elementos derrotables del elemento elegido por el usuario.
 
-        if eligeUsuario["id"] != eligePc["id"]:
-            if num == eligePc["id"]:
+        if eligeUsuario["id"] != eligePc["id"]: ## Descarta de inmediato si hay situacion de empate o no
+            if num == eligePc["id"]: ## Si el elemento que recorre en ese momento encuentra el id del elemento rival, se declara victoria para usuario.
                 print("Ganaste!!! ", eligeUsuario["nombre"], "le gana a", eligePc["nombre"], "\n")
-                
                 ## Actualizamos estadisticas de victoria
                 partidas += 1
                 analiticas["partidas"] = partidas
                 victorias += 1
                 analiticas["victorias"] = victorias
                 break
-            elif eligeUsuario["id"] == eligePc["ganaContra"][i]:
-                print("Perdiste :/ ", eligeUsuario["nombre"], "pierde contra", eligePc["nombre"], "\n")
+            elif eligeUsuario["id"] == eligePc["ganaContra"][i]: ## En caso de no encontrar victoria, checkea si el id de nuestro elemento se encuentra
+                                                                 ## en la lista de id's derrotables por el elemento rival.    
+                print("Perdiste :/ ", eligeUsuario["nombre"], "pierde contra", eligePc["nombre"], "\n") ## En dicho caso, derrota para usuario.
                 
                 ## Actualizamos estadisticas de derrota
                 partidas += 1
@@ -151,8 +152,9 @@ while True:
                 derrotas += 1
                 analiticas["derrotas"] = derrotas
                 break
-            else:
-                i += 1
+            else: 
+                i += 1 ## Si no encuentra nada, incrementa variable 'i', para que en siguiente vuelta de bucle, compare con el proximo id en lista de
+                       ## elementos derrotables por el rival.
         else:
             ## Actualizamos estadisticas de empate
             partidas += 1
